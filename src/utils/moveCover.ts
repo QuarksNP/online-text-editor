d87d7e4fb600c24a304e689796c9e391e3432f3c
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-07-29 19:12:30 -0400

    fixing errors path

--- src/utils/moveCover.ts
+++ src/utils/moveCover.ts
@@ -1,8 +1,11 @@
-export function moveLoadCover(childElementRef: React.RefObject<HTMLElement>, coverRef: React.RefObject<HTMLElement>) {
+export function moveLoadCover(
+    childElementRef: React.RefObject<HTMLElement>, 
+    coverRef: React.RefObject<HTMLElement>) {
+        
     const childElement = childElementRef.current
     const cover = coverRef.current
 
-    if(cover != null && childElement != null) {
+    if (cover != null && childElement != null) {
         cover.appendChild(childElement)
     }
 }
